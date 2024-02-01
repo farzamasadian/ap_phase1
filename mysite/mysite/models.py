abc15afe14a4a3c7d75dc2c046177b9039f63b17
Farzam Asadian <140963605+farzamasadian@users.noreply.github.com>
2024-02-02 01:25:56 +0330

    Add files via upload

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -2,11 +2,26 @@ from datetime import datetime, timedelta
 from enum import Enum
 import random
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager, Group, Permission
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, email, password, **extra_fields)
 
 class User(AbstractUser):
     USER_TYPE_CHOICES = (
@@ -18,32 +33,44 @@ class User(AbstractUser):
     otp = models.CharField(max_length=6, blank=True, null=True)
     otp_expiry = models.DateTimeField(blank=True, null=True)
 
+    objects = CustomUserManager()
+
     def generate_otp(self):
-        """
-        Generates a One Time Password for the user and sets its expiry time.
-        """
         self.otp = str(random.randint(100000, 999999))
-        self.otp_expiry = timezone.now() + timedelta(minutes=5)  # OTP expires in 5 minutes
+        self.otp_expiry = timezone.now() + timedelta(minutes=5)
         self.save()
         return self.otp
 
     def check_otp(self, input_otp):
-        """
-        Check if the input OTP is valid.
-        """
         if self.use_otp and self.otp == input_otp and timezone.now() < self.otp_expiry:
             return True
         return False
 
     def update_profile(self, new_email=None, new_password=None):
-        """
-        Updates the user's email or password.
-        """
         if new_email:
             self.email = new_email
         if new_password:
             self.set_password(new_password)
         self.save()
+
+    # Add related_name to avoid clashes
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_name='custom_user_groups'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name='custom_user_permissions'
+    )
 
 
 class Clinic(models.Model):
